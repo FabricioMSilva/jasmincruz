dd5e660351e648869123c226ca9d62c6a4ce1657
FABRICIO MARTINS DA SILVA <87395533+FabricioMSilva@users.noreply.github.com>
2024-01-28 19:40:52 -0300

    2

--- src/components/Menu/BotãoWhatsZap/BtnZap.jsx
+++ src/components/Menu/BotãoWhatsZap/BtnZap.jsx
@@ -1,6 +1,6 @@
 import Logo from '../BotãoWhatsZap/whats.png';
 import Jasmin from '../../Imagens/LogoJasmin.png';
-import { useForm } from 'react-hook-form';
+import ReactWhatsapp from 'react-whatsapp';
 import { KitZap,BtnZapZap, CaixaTexto, Cabecalho, InputTexto } from '../../styled/BtnwhatsZap';
 import { AiOutlineSend } from "react-icons/ai";
 import { IoMdCloseCircleOutline } from "react-icons/io";
@@ -9,8 +9,14 @@ export default BtnZap => {
     const [showElement, setShowElement] = useState(false)
     const Ativo = () => setShowElement(true)
     const Desativo =()=>setShowElement(false)
-    const { register, handleSubmit } = useForm();
-    return (
+
+    const[Menssagem,setMenssagem] = useState("");
+  
+    const inputControlado = (event)=>{
+          setMenssagem(event.target.value);
+};
+
+return( 
         <KitZap>
            {showElement? 
             <CaixaTexto >
@@ -20,16 +26,13 @@ export default BtnZap => {
                     <IoMdCloseCircleOutline size={25} onClick={Desativo} />
                 </Cabecalho>
                 <InputTexto >
-                    <input type='text'{...register("Telefone")}
-                    
-                    placeholder='Digite sua mensagem'
-                    ></input>
-                    <div >
-                    <AiOutlineSend size={25}  /></div>
+                    <input type='text' onChange={inputControlado} placeholder='Digite sua mensagem'>
+                    </input>
+                    <a target="_blank" href={(`https://wa.me/5524981112790/?text=Oi Jasmin ${Menssagem}`)} onClick={Desativo} > 
+                    <AiOutlineSend size={25}   /></a>
                 </InputTexto>
             </CaixaTexto>
             :null}
-
             <BtnZapZap>
                 <img alt="" src={Logo}onClick={Ativo}></img>
                 <div class="wrapper"></div>
